36593ef2860e6b771fd53668a1b6629b72b76b3d
schulzf <fschulzfurtado@gmail.com>
2022-11-20 16:18:13 +0100

    tsting

--- step1.py
+++ step1.py
@@ -1,3 +1,5 @@
+from IPython import get_ipython
+
 get_ipython().run_line_magic('pip', 'install -q https://github.com/TheLastBen/fast-stable-diffusion/raw/main//precompiled/Non-Colab/Paperspace/A4000/xformers-0.0.14.dev0-cp39-cp39-linux_x86_64.whl')
 
 print('[1;32mDONE !')
